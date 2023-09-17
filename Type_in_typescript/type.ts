c6a4358c3df26fec480df837ccd412e81af52839
Mudasir Irshad <mudasirirshad47@gmail.com>
2023-09-17 10:49:45 +0500

    updated

---
+++ Type_in_typescript/type.ts
@@ -0,0 +1,15 @@
+
+// making "User" as a type which is assigned to the newUser object
+type User={
+    name:string,
+    greet:()=> string //defining function with retrun type string
+}
+
+let newUser:User={
+    name:"Mudasir",
+    greet:function(){
+        return `Welcome ${this.name}`;
+    }
+}
+
+console.log(newUser.greet());
