2f481ca1ee73343ac7b3a49b912929854717fc56
Mudasir Irshad <mudasirirshad47@gmail.com>
2023-09-17 08:21:36 +0500

    Updated

---
+++ Variable_declaration/number.ts
@@ -0,0 +1,16 @@
+// this is how we declare the number or any vaiabel type in typescript
+
+let a:number
+let b:number
+
+// ----------------------------------------------------------------
+
+a=50
+b=100
+
+// ----------------------------------------------------------------
+
+// a="Mudasir Irshad" 
+//if we pass string or ther type in number variable then typescript will show error but on the other hand javascript can't show error in compile time.
+
+console.log(a+b)
