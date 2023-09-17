b146d2d114529d64b51b0c37b1a5575afb0a19df
Mudasir Irshad <mudasirirshad47@gmail.com>
2023-09-17 08:52:47 +0500

    updated

--- Function/function.ts
+++ Function/function.ts
@@ -4,3 +4,9 @@ function greeting(name:string,age:number):string {
 
 // [function greeting(name:string):string ] here :string shows the return type of the function
 
+
+let goodDay= (name:string):string =>{
+    return `Good day to you Mr. ${name}`
+}
+
+// using arrow function in typescript
