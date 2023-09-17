3895410997eb42aadf1965e8118cf9b7437fe82f
Mudasir Irshad <mudasirirshad47@gmail.com>
2023-09-17 08:44:21 +0500

    updated

---
+++ Objects/object.ts
@@ -0,0 +1,10 @@
+// objects in typescript are same as javascript objects
+
+let user={
+    name:"Mudasir Irshad",
+    age:21,
+    occupation:"Student",
+    gmail:"mudasirirshad47@gmail.com"
+}
+
+console.log(user.name);
