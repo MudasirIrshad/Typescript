cc5b24729bb783ca9d096ed23c363586cfe12105
Mudasir Irshad <mudasirirshad47@gmail.com>
2023-09-17 08:24:55 +0500

    updated

---
+++ Variable_declaration/string.ts
@@ -0,0 +1,9 @@
+let myName:string  //One way of declaring string in TS
+myName="Mudasir Irshad"
+console.log(myName);
+
+//----------------------------------------------------------------
+
+
+let userName:string ="Mudasir Irshad" // Other way of declaring string in TS
+console.log(userName)
