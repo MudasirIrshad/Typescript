f91ac2069de3caf8c2180c50a270b55c8bac9f92
Mudasir Irshad <mudasirirshad47@gmail.com>
2023-09-17 08:49:20 +0500

    udpade

---
+++ Objects/Function/function.ts
@@ -0,0 +1,6 @@
+function greeting(name:string,age:number):string {
+    return `Hello ${name}, your age is ${age}`
+}
+
+// [function greeting(name:string):string ] here :string shows the return type of the function
+
