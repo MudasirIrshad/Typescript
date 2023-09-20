4955e0154e36853f9c83d414928c40ff76475534
Mudasir Irshad <mudasirirshad47@gmail.com>
2023-09-20 21:43:33 +0500

    Tuple

---
+++ Tuple/tuple.ts
@@ -0,0 +1,7 @@
+// tuple is just assigning the items and their data types to array.
+
+let tup:[string,number]=["mudasir",20]
+console.log(`Name: ${tup[0]}`);
+console.log(`Age: ${tup[1]}`);
+
+// we can not assign more than 2 values to this tup[].
